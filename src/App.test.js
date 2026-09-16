98b394df3fd01999dd4fb100d6dfcf9802b03ce4
Ricky Fielding <ricky.fielding@gmail.com>
2026-09-15 21:26:56 -0700

    yet another commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/We don't need no education!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/We don't need no education!/i);
   expect(linkElement).toBeInTheDocument();
 });
